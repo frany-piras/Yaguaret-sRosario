56c2104b0decb4a39272d2e5a46663cfe2d1adfc
Frany Piras <115931541+frany-piras@users.noreply.github.com>
2022-11-25 08:37:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+(function(){
+
+    $(function () {
+
+        $('#boton-ventana').on('click', function () {
+        $('#ventana-modal').modal()
+
+        });
+    });
+
+    $(function () {
+
+        $('#boton-ventana1').on('click', function () {
+            $('#ventana-modal1').modal()
+
+        });
+    });
+
+    $(function () {
+
+        $('#boton-ventana2').on('click', function () {
+            $('#ventana-modal2').modal()
+
+        });
+    });
+
+    $(function () {
+
+        $('#boton-ventana3').on('click', function () {
+            $('#ventana-modal3').modal()
+
+        });
+    });
+
+    $(function () {
+
+        $('#boton-ventana4').on('click', function () {
+            $('#ventana-modal4').modal()
+
+        });
+    });
+
+    $(function () {
+
+        $('#boton-ventana5').on('click', function () {
+            $('#ventana-modal5').modal()
+
+        });
+    });
+}());
+
